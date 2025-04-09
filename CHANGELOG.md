ac1288fd2993447e0464630709f5305950192c61
BIA <alexandre.bianchi@nag.ch>
2025-04-09 12:28:08 +0200

    Added internal version constant

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 # Changelog
 
+## 2.0.0
 - Update MSAL Android version to `6.0.0` and MSAL iOS version to `1.9.0`
 
 - Update MSAL Android version to `5.10.2` and MSAL iOS version to `1.8.1`
--- lib/msal_flutter.dart
+++ lib/msal_flutter.dart
@@ -1,3 +1,5 @@
 /// A wrapper for the Microsoft Authentication Library packages available on Android and iOS
 export 'src/public_client_application.dart';
 export 'src/exceptions/msal_exceptions.dart';
+
+const String msalFlutterInternalVersion = '2.0.0';
